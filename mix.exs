69b39839c9cd14180b209a2c9a7f776a469b677b
Mat Trudel <mat@geeky.net>
2022-11-03 14:23:31 -0400

    Make bandit and cowboy deps optional

--- mix.exs
+++ mix.exs
@@ -28,9 +28,9 @@ defmodule WebSockAdapter.MixProject do
   defp deps do
     [
       {:websock, "~> 0.4.2"},
-      {:bandit, "~> 0.5.9"},
       {:plug, "~> 1.14.0"},
-      {:plug_cowboy, "~> 2.6"},
+      {:bandit, "~> 0.5.9", optional: true},
+      {:plug_cowboy, "~> 2.6", optional: true},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:credo, "~> 1.0", only: [:dev, :test], runtime: false}
